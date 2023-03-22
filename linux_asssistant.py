2e1969f299252a1922d470e7954562eeb3f00646
Taraszhygal <zhygalt@gmail.com>
2023-03-22 22:12:48 +0200

    start method updated

--- linux_asssistant.py
+++ linux_asssistant.py
@@ -4,8 +4,21 @@ import subprocess
 from   subprocess import check_call, CalledProcessError, STDOUT
 import os
 import importlib
+from tarfile import RECORDSIZE
 
+def runcmd(cmd, verbose = False, *args, **kwargs):
 
+    process = subprocess.Popen(
+        cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True,
+        shell = True
+    )
+    std_out, std_err = process.communicate()
+    if verbose:
+        print(std_out.strip(), std_err)
+    pass
 
 def install():
     print("Installing...")
@@ -16,11 +29,11 @@ def install():
         reqs = subprocess.check_output([sys.executable, '-m', 'pip','freeze'])
         installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
         print(installed_packages)
-        subprocess.check_call(['apt-get', 'install', '-y', 'python3-pyaudio unzip'], stdout=open(os.devnull,'wb'), stderr=STDOUT) 
-        print("pyaudio installed")
+        runcmd('apt install -y python3-pyaudio unzip') 
+        #print("pyaudio installed")
         #download ukr model audio semple
-        subprocess.check_call(['wget', 'https://alphacephei.com/vosk/models/vosk-model-small-uk-v3-nano.zip'], stderr=STDOUT)
-        subprocess.check_call(['unzip', 'vosk-model-small-uk-v3-nano.zip'], stderr=STDOUT)
+        runcmd('wget https://alphacephei.com/vosk/models/vosk-model-small-uk-v3-nano.zip')
+        runcmd('unzip vosk-model-small-uk-v3-nano.zip && rm vosk-model-small-uk-v3-nano.zip')
     except CalledProcessError as e:
         print(e.output)
 
@@ -28,8 +41,23 @@ def start():
     vosk = importlib.import_module('vosk')
     model = getattr(vosk, 'Model')
     kr = getattr(vosk, 'KaldiRecognizer')
+    pyaudio = importlib.import_module('pyaudio')
+    print("Starting...")
+        
+    #read the model
+    ukrm = model('./vosk-model-small-uk-v3-nano')
+    recognizer = kr (ukrm, 16000)
+    
+    #recognize micro
+    cap = pyaudio.Pyaudio()
+    stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+    stream.start_stream()
     while True:
-        print("Starting...")
+        data = stream.read(4096)
+        if recognizer.AcceptWaveform(data):
+            print(recognizer.Result())
+        
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Install and start application")
